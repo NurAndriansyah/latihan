8a02f650278f23a2c02aac80c50fc35ff2461d72
NurAndriansyah <nandreansyah85@gmail.com>
2024-03-14 21:58:32 +0700

    Menambahkan file learn.dart

---
+++ learn.dart
@@ -0,0 +1,11 @@
+import 'dart:io';
+
+void main() {
+  stdout.write("Nama Anda: ");
+  String? name = stdin.readLineSync();
+
+  stdout.write("Jurusan Anda: ");
+  String? jurusan = stdin.readLineSync();
+
+  print("Halo $name, jurusan Anda $jurusan");
+}
